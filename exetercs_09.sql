bdcd87592cae3f81c0ec455812ca36d7668213b3
SowmyaMahadevan <sowmya@exeterpremedia.com>
2016-09-29 11:21:31 +0530

    Add files via upload

---
+++ exetercs_09.sql
@@ -0,0 +1,14 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : CMS30
+ Source Server Version : 50544
+ Source Host           : cms30.ama.uk.com
+ Source Database       : exetercs_09
+
+ Target Server Version : 50544
+ File Encoding         : utf-8
+
+ Date: 09/29/2016 09:55:34 AM
+*/
+
